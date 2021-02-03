d731b9969767b7b5adb215567c19d437577e9fbc
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-03 08:13:55 +0700

    ADD:start up files

---
+++ script.js
@@ -0,0 +1,29 @@
+let beats = {
+  65: {
+    beat: new Beat("./assets/Piano Chord 331.mp3"),
+  },
+  83: {
+    beat: new Beat("./assets/Piano Chord 209.mp3"),
+  },
+  68: {
+    beat: new Beat("./assets/Piano Chord 208.mp3"),
+  },
+  70: {
+    beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+  },
+  71: {
+    beat: new Beat("./assets/Drum Snare Roll.mp3"),
+  },
+  72: {
+    beat: new Beat("./assets/PREL Musical 57.mp3"),
+  },
+  74: {
+    beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+  },
+  75: {
+    beat: new Beat("./assets/Musical Compos 33.mp3"),
+  },
+  76: {
+    beat: new Beat("./assets/Musical Orches 4.mp3"),
+  },
+};
